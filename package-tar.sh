035dcf83fa3bcb32bbce352d649f791d2a2dc1e4
Mark Kimsal <metrofindings@gmail.com>
2017-01-28 09:02:41 -0500

    Make blank var dirs when packaging

--- package-tar.sh
+++ package-tar.sh
@@ -20,6 +20,8 @@ cp  composer.json $PACKAGE_ROOT/opt/tanc/www/
 cp  composer.lock $PACKAGE_ROOT/opt/tanc/www/
 
 mkdir $PACKAGE_ROOT/opt/tanc/www/var/
+mkdir -p $PACKAGE_ROOT/opt/tanc/www/var/{db,cache,sess,templates}
+
 for x in `find var/ -type d`
 do
 	mkdir -p $PACKAGE_ROOT/opt/tanc/www/$x
