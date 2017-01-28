aafe445a177487433cb585cda802e907a362dbee
Mark Kimsal <metrofindings@gmail.com>
2017-01-28 11:03:30 -0500

    Sometimes you just want to put a script tag

--- src/template/extrajs.php
+++ src/template/extrajs.php
@@ -3,15 +3,16 @@
 class Template_Extrajs {
 
 	public function template($response, $template_section) {
-		$list = $response->extrajs;
+		$list = $response->extraJs;
 		if (!is_array($list)) {
-			echo'
-<script type="text/javascript" href="'.$list.'"></script>
-';
-			return;
+			$list = array($list);
 		}
 
 		foreach ($list as $_idx => $_url) {
+			if (strstr($_url, '<script')) {
+				echo $_url;
+				continue;
+			}
 			if (substr($_url, 0, 1) == '/') {
 				$url = $_url;
 			} else if (substr($_url, 0, 4) == 'http') {
