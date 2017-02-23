c5725ce4a89baa6cee2ac3134d1ea11b893502cb
Mark Kimsal <metrofindings@gmail.com>
2017-02-23 15:11:20 -0500

    Show keypad on main screen

---
+++ src/main/main.php
@@ -0,0 +1,12 @@
+<?php
+
+class Main_Main {
+
+
+	public function mainAction($request, $response, $kernel) {
+		$kernel->iCanHandle('process', 'kp/main.php::mainAction');
+		$kernel->iCanHandle('output', 'kp/main.php');
+		$request->appName = 'kp';
+//		_set('template.main.file', 'kp/views/main_main.tpl');
+	}
+}
