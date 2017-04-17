7cd9cee325793fd3308cb541a451481f0f40ebb0
Mark Kimsal <metrofindings@gmail.com>
2017-04-17 08:48:00 -0400

    Allow native json exceptions

--- src/main/whoopsexception.php
+++ src/main/whoopsexception.php
@@ -1,14 +1,17 @@
 <?php
 class Main_Whoopsexception {
 
-	public function exception() {
+	public function exception($request) {
 		$run     = new Whoops\Run;
-		$handler = new Whoops\Handler\PrettyPageHandler;
-
-		// Set the title of the error page:
-		$handler->setPageTitle("errorWhoops! There was a problem.");
-		$response = _make('response');
-		$handler->addDataTable('template vars', $response->sectionList);
+		if ($request->isAjax) {
+			$handler = new Whoops\Handler\JsonResponseHandler;
+		} else {
+			$handler = new Whoops\Handler\PrettyPageHandler;
+			$handler->setPageTitle("errorWhoops! There was a problem.");
+			// Set the title of the error page:
+			$response = _make('response');
+			$handler->addDataTable('template vars', $response->sectionList);
+		}
 
 		$run->pushHandler($handler);
 		$run->handleException( _get('last_exception') );
