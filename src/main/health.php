dcad721f6507a1ed172b3ebf3d128102a4cf5a83
Mark Kimsal <metrofindings@gmail.com>
2017-09-24 11:28:19 -0400

    Fix health check script

--- src/main/health.php
+++ src/main/health.php
@@ -14,9 +14,17 @@ class Main_Health {
 			$response->addInto('user-message', ['msg'=>'Cannot communicate with message queue.', 'type'=>'error']);
 			return;
 		}
+		//new web socket server routinely drains display queue
+		//so we cannot determine if there's trouble when the display queue
+		//is empty.
+		//TODO: we could maybe look at how many workers are watching
+		//the default queue
+		//or we could check the systemd service serial-comm
+		/*
 		if (!$ready) {
 			$response->addInto('user-message', ['msg'=>'Trouble with message queue.', 'type'=>'error']);
 		}
+		 */
 	}
 
 
